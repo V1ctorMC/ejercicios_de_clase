94d84fdab223431ca476aebcfc82774e691e881f
V1ctorMC <molina.chavero.victor@iescamas.es>
2024-10-01 10:44:18 +0200

    Add files via upload

---
+++ 15%_propina.py
@@ -0,0 +1,7 @@
+#Esto calcula el 15% de propina
+print("¿Cuanto a costado tu cuenta?")
+euro = int(input())
+TASA = float(0.15)
+total = euro * TASA
+#Hacemos el calculo
+print("Tu propina debe ser de",total,"euros")
