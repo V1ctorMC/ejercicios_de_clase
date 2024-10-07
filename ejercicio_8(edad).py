912eebebeda260a61ce1fc6f9d47c14a3767be7c
V1ctorMC <molina.chavero.victor@iescamas.es>
2024-10-07 10:02:15 +0200

    Add files via upload

---
+++ ejercicio_8(edad).py
@@ -0,0 +1,7 @@
+#Este programa te calcula tu edad
+print("¿En que año naciste?")
+añoN = int(input())
+print("¿En que año estamos?")
+añoA = int(input())
+año = añoA - añoN
+print("Tu edad actual es", año,"años")
