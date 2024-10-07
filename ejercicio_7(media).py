64c4dca4e74998a61d187e2277e255aece2b8f23
V1ctorMC <molina.chavero.victor@iescamas.es>
2024-10-07 10:02:38 +0200

    Add files via upload

---
+++ ejercicio_7(media).py
@@ -0,0 +1,9 @@
+#Este programa hace la nota media entre tres notas
+print("¿Cual es tu primera nota?")
+nota1 = float(input())
+print("¿Cual es tu segunda nota?")
+nota2 = float(input())
+print("Cual es tu tercera nota?")
+nota3 = float(input())
+media = (nota1 + nota2 + nota3) / 3
+print("Tu nota media es", media)
