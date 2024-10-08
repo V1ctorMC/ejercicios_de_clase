cdab33313738ca8dad54b5ccd4d7228d213cfb16
V1ctorMC <molina.chavero.victor@iescamas.es>
2024-10-08 10:56:33 +0200

    Add files via upload

---
+++ ejercicio_9(temperatura).py
@@ -0,0 +1,6 @@
+#Este programa pasa de grados celsius a fahrenheit o kelvin
+print("Escribe una temperatura en grados celsius")
+GC = float(input())
+GF = (GC * 9 / 5) + 32
+GK =  GC + 273.15
+print(GC,"GC son", GF,"GF y", GK,"GK")
