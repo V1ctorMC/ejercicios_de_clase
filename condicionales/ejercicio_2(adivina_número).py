bf66fb44a4b2b04fff2d1527d9f67845bdb384b4
V1ctorMC <molina.chavero.victor@iescamas.es>
2024-10-14 09:50:05 +0200

    Add files via upload

---
+++ condicionales/ejercicio_2(adivina_número).py
@@ -0,0 +1,8 @@
+#Este programa consiste en adivina un numero del 1 al 10
+númeroA = 8
+print("Adivina el número del 1 al 10")
+número = int(input())
+if(número == númeroA):
+    print("Has acertado!!")
+else:
+    print("Intentalo de nuevo")
