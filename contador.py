30541a8df8bda0f8afea553b93da4fff75b71449
V1ctorMC <molina.chavero.victor@iescamas.es>
2024-12-17 10:37:54 +0100

    Add files via upload

---
+++ contador.py
@@ -0,0 +1,6 @@
+contador = 0
+print("¿Hasta que numero quieres q cuente?")
+n = int(input())
+while(contador <= n):
+    print(contador)
+    contador = contador + 1
---
+++ tablasmultiplicar.py
@@ -0,0 +1,6 @@
+contador = 0
+print("¿De que numero te hago la tabla de multiplicar?")
+n = int(input())
+while(contador <= 10):
+    print (n,"x",contador ,"=",contador * n)
+    contador = contador + 1
